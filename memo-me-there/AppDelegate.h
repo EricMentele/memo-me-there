b8fa642e765c208a0b1d18163e513e5607b734a5
Eric Mentele <diviningsage@gmail.com>
2015-02-02 17:18:14 -0800

    Initial Commit

---
+++ memo-me-there/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  memo-me-there
+//
+//  Created by Eric Mentele on 2/2/15.
+//  Copyright (c) 2015 Eric Mentele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ memo-me-there/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  memo-me-there
+//
+//  Created by Eric Mentele on 2/2/15.
+//  Copyright (c) 2015 Eric Mentele. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
